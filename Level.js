a7f90157a071c63e6399f5db19ce0c60abd266a5
MTLRTA <githubers1@gmail.com>
2025-03-15 21:14:02 +0200

    Update - 2(добавлены текстуры)

--- Level.js
+++ Level.js
@@ -1,28 +1,50 @@
 const boardSize = 5;
 
-const wall = 1;   // стена
-const box = 2;    // коробка
-const target = 3; // цель
-const player = 4; // игрок
-const empty = 0;  // пустая клетка
+const wall = 1;   
+const box = 2;    
+const target = 3; 
+const player = 4; 
+const empty = 0;  
 
 const Level = [
     wall, wall, wall, wall, wall, empty, empty, empty, empty,
     wall, empty, empty, empty, wall, empty, empty, empty, empty,
     wall, empty, box, empty, wall, empty, wall, wall, wall,
-    wall, empty, box, empty, wall, empty, wall, target, wall,
+    wall, empty, box, empty, wall, empty, wall, player, wall,
     wall, wall, wall, empty, wall, wall, wall, empty, wall,
-    wall, player, empty, empty, empty, empty, empty, empty, wall,
-    wall, player, empty, empty, empty, empty, wall, empty, wall,
+    wall, target, empty, empty, empty, empty, empty, empty, wall,
+    wall, target, empty, empty, empty, empty, wall, empty, wall,
     wall, empty, empty, empty, empty, empty, wall, wall, wall,
     wall, wall, wall, wall, wall, wall, wall, wall, wall,
 ];
 
-console.log(Level);
-
 const board = document.getElementById('gamelevel');
 
-const elements = {
+function drawLevel() {/* Метод для построения сетки уровня путем перебора каждого элемента*/
+    Level.forEach((cell) => {
+        const cellDiv = document.createElement('div');
 
+        switch (cell) {
+            case wall:
+                cellDiv.classList.add('wall');
+                break;
+            case box:
+                cellDiv.classList.add('box');
+                break;
+            case target:
+                cellDiv.classList.add('target');
+                break;
+            case player:
+                cellDiv.classList.add('player');
+                break;
+            case empty:
+                cellDiv.classList.add('empty');
+                break;    
+        }
 
+        board.appendChild(cellDiv);
+    });
 }
+
+drawLevel();
+
